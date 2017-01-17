956a2a26910b63044796e6882f1dae16a4e5f4b0
DESKTOP-TVC0PFL\DUC THANG <ducthangwru97@gmail.com>
2017-01-18 01:50:03 +0700

    add button login facebook

--- app/src/main/java/shipdoandem/amytateam/org/shipdoandem/LoginActivity.java
+++ app/src/main/java/shipdoandem/amytateam/org/shipdoandem/LoginActivity.java
@@ -1,26 +1,143 @@
 package shipdoandem.amytateam.org.shipdoandem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button bt_loginFacebook;
+
+    private LoginActivity loginActivity;
+    private CallbackManager callbackManager;
+    private FacebookCallback<LoginResult> loginResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getReferences();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        loginActivity = this;
+        initFaceBook();
+        LoginManager.getInstance().registerCallback(callbackManager, loginResult);
+        bt_loginFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginFaceBook();
+            }
+        });
         printKeyHash(this);
     }
 
-    public static String printKeyHash(Activity context) {
+    private void getReferences() {
+        bt_loginFacebook = (Button) findViewById(R.id.bt_login_with_facebook);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //Login facebook with permisstion
+    public void loginFaceBook() {
+        LoginManager.getInstance().logInWithReadPermissions(loginActivity, Arrays.asList("public_profile", "user_friends","email"));
+    }
+
+
+
+    //Hàm check login facebook
+    public boolean isLoggedInFaceBook() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    //Lấy Avatar
+    public URL extractFacebookIcon(String id) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            URL imageURL = new URL("http://graph.facebook.com/" + id
+                    + "/picture?type=large");
+            return imageURL;
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    public void initFaceBook () {
+        loginResult = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Login thành công xử lý tại đây
+                GraphRequest request = GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object,
+                                                    GraphResponse response) {
+                                // Application code
+                                String name = object.optString(getString(R.string.name));
+                                String id = object.optString(getString(R.string.id));
+                                String email = object.optString(getString(R.string.email));
+                                String link = object.optString(getString(R.string.link));
+                                URL imageURL = extractFacebookIcon(id);
+                                Log.d("name: ",name);
+                                Log.d("id: ",id);
+                                Log.d("email: ",email);
+                                Log.d("link: ",link);
+                                Log.d("imageURL: ",imageURL.toString());
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString(getString(R.string.fields), getString(R.string.fields_name));
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        };
+    }
+
+    //Get keyHash
+    public String printKeyHash(Activity context) {
         PackageInfo packageInfo;
         String key = null;
         try {
